3793b420ba6465a83fb1dbf9a51e0db3f666a93e
Sulkifli Asmunandar <kflmattukz@gmail.com>
2023-09-08 21:58:49 +0800

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-
-
 export default function Home() {
-  return (
-    <div>Hello from</div>
-  )
+  return <div>Hello from</div>;
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Button, Layout, Space } from "antd";
+import Link from "next/link";
+import { Button, Layout } from "antd";
 import { SignIn, SignOut } from "@icons";
-import { icons } from "antd/es/image/PreviewGroup";
 const { Header } = Layout;
 
 export function Nav() {
@@ -11,9 +11,9 @@ export function Nav() {
         <nav className="flex justify-between items-center sticky">
           <h2 className="text-white text-xl font-bold">Next Project</h2>
           <div className="links text-white space-x-5">
-            <a href="/">Home</a>
-            <a href="/">blogs</a>
-            <a href="/">About</a>
+            <Link href="/">Home</Link>
+            <Link href="/blog">blogs</Link>
+            <Link href="/about">About</Link>
           </div>
           <div className="actions space-x-2 flex">
             <Button
